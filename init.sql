12c8aeceb61ea0b0a0aaa6decabb15dd9a4c68d0
Matthew Hollick <matthew@hedgehoganalytics.uk>
2019-10-06 11:26:10 +0000

    latest

--- init.sql
+++ init.sql
@@ -1,14 +1,15 @@
-CREATE TABLE IF NOT EXISTS default.graphite_reverse (
-  Path String,
-  Value Float64,
-  Time UInt32,
-  Date Date,
+CREATE TABLE graphite ( 
+  Path String,  
+  Value Float64,  
+  Time UInt32,  
+  Date Date,  
   Timestamp UInt32
 ) ENGINE = GraphiteMergeTree('graphite_rollup')
 PARTITION BY toYYYYMM(Date)
 ORDER BY (Path, Time);
 
-CREATE TABLE IF NOT EXISTS default.graphite_index (
+-- optional table for faster metric search
+CREATE TABLE graphite_index (
   Date Date,
   Level UInt32,
   Path String,
@@ -16,13 +17,3 @@ CREATE TABLE IF NOT EXISTS default.graphite_index (
 ) ENGINE = ReplacingMergeTree(Version)
 PARTITION BY toYYYYMM(Date)
 ORDER BY (Level, Path, Date);
-
-CREATE TABLE IF NOT EXISTS default.graphite_tagged (
-  Date Date,
-  Tag1 String,
-  Path String,
-  Tags Array(String),
-  Version UInt32
-) ENGINE = ReplacingMergeTree(Version)
-PARTITION BY toYYYYMM(Date)
-ORDER BY (Tag1, Path, Date);
