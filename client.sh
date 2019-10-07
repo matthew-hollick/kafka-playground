6ddfc4b76721dd608287dfc87a426c1f7ed09a15
Matthew Hollick <matthew@mayan-it.co.uk>
2019-10-08 00:55:08 +0100

    Update client.sh

--- client.sh
+++ client.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker-compose exec clickhouse bash -c "
+docker-compose -f docker-compose-metrics.yml exec clickhouse bash -c "
     export HOME=/var/lib/clickhouse/
     exec clickhouse client
 "
